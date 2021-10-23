f515d2ac899c325e3b9b8eefffaca10ad84ce439
Vincent Clausing-Lage <vincent.clausing.lage@gmail.com>
2021-10-23 20:24:27 +0200

    algorithm nlp

--- greek-nlp/main_tokenizer.py
+++ greek-nlp/main_tokenizer.py
@@ -1,65 +1,67 @@
 import json
-from os import altsep
 import re
 from tokenizer import morphology_generator
 from tokenizer import query_tokens_generator
 from tokenizer import text_preparer
 from tokenizer import noise_terms
-from tokenizer import lemma_generator
+from tokenizer import lemma_generator as lem
 
-# To Do
-# ENDUNGEN IN JSON !!! ergänzen
-
-# generate TOKENS (lemma) for QUERY
-query_tokens = morphology_generator.generateDecl(query_tokens_generator.stems_decl_full)
-print("1. query tokens genreated")
 # PREPARE text (remove DIACRITICS)
 text = text_preparer.text_preparer("tokenizer/input.txt")
-print("2. text prepared")
+print("1. text loaded and prepared")
 with open("normalizedText.txt", "w") as f:
   f.write(text)
 # PREAPARE text (remove NOISE)
 text = noise_terms.removeNoise(text, noise_terms.noise_terms)
 print("... and noise removed from text")
 
-# prepare QUERY LISTS
-angry_list = query_tokens["angry"]["aDecl"] + query_tokens["angry"]["oDecl"] + query_tokens["angry"]["kDecl"]
-nasty_list = query_tokens["nasty"]["aDecl"] + query_tokens["nasty"]["oDecl"] + query_tokens["nasty"]["kDecl"]
-affectionate_list = query_tokens["affectionate"]["aDecl"] + query_tokens["affectionate"]["oDecl"] + query_tokens["affectionate"]["kDecl"]
-nice_list = query_tokens["nice"]["aDecl"] + query_tokens["nice"]["oDecl"] + query_tokens["nice"]["kDecl"]
+angry_subst = [("φόβος", "φόβου"), ("πόλεμος", "πολέμου"), ("βλάβη", "βλάβης"), ("ὀργή", "ὀργῆς"), ("αἰσχύνη", "αἰσχύνης"), ("βία", "βίας")] ; angry_adj = [("στυγερός", "στυγεροῦ"), ("ἐχθρός", "ἐχθροῦ")]
+nasty_subst = [("ψόγος", "ψόγου"), ("δεῖμα", "δείματος")]; nasty_adj = [("κακός", "κακοῦ")]
+affectionate_subst = [("φίλος", "φίλου"), ("γάμος", "γάμου"), ("σωτήρ", "σωτήρος")]; affectionate_adj = []
+nice_subst = [("χαρις", "χάριτος")]; nice_adj = [("καλός", "καλοῦ")]
 
-# QUERY text with tokens
+angry = lem.fillLemmata(angry_subst) + lem.fillLemmata(angry_adj)
+nasty = lem.fillLemmata(nasty_subst) + lem.fillLemmata(nasty_adj)
+affectionate = lem.fillLemmata(affectionate_subst) + lem.fillLemmata(affectionate_adj)
+nice = lem.fillLemmata(nice_subst) + lem.fillLemmata(nice_adj)
+
+lemmata = {"angry": angry, "nasty": nasty, "affectionate": affectionate, "nice": nice}
+
+def lemmatizeText(text, lemmata):
+  all_lemmata = lemmata["angry"] + lemmata["nasty"] + lemmata["affectionate"] + lemmata["nice"]
+  for element in all_lemmata:
+    for lemma in element:
+      # print(lemma)
+      text = re.sub(rf"\b{lemma}\b", element[1], text) # text.replace(lemma, element[1])
+  return text
+print("2. lemmatizing text")
+lemmatized_text = lemmatizeText(text, lemmata)
+print("writing file...")
+with open("lemmatizedText.txt", "w") as f:
+  f.write(lemmatized_text)
+
 def findToken(text, query_list):
   result = []
+  count = 0
   for query in query_list:
-    query = query.split()
-    count = 0
-    for x in query:
-      count += len(re.findall(rf'\b{x}\b', text))
-    result.append({"query": query[0], "count": count})
-  return result
-print("3. begin query")
-query_tokens["angry"]["result"] = findToken(text, angry_list)
-query_tokens["nasty"]["result"] = findToken(text, nasty_list)
-query_tokens["affectionate"]["result"] = findToken(text, affectionate_list)
-query_tokens["nice"]["result"] = findToken(text, nice_list)
+    for el in query:
+      count += len(re.findall(rf"\b{el}\b", text))
+    result.append(query[0])
+  return {"tokens": result, "result": count}
+print("3. finding tokens")
+results = {"angry": findToken(text, angry), "nasty": findToken(text, nasty), "affectionate": findToken(text, affectionate), "nice": findToken(text, nice)}
 
-print("4. writing file")
-with open("tokenizer/queryTokens.json", "w", encoding='utf8') as f:       # write RESULTS to FILE (necessary?)
-  query_tokens = json.dumps(query_tokens, ensure_ascii=False, indent=2)
-  f.write(str(query_tokens))
-
-lemmatized_text = lemma_generator.replaceLemmata(text, lemma_generator.lemmata_list)
-print("5. testing word frequencies")
-def word_count(query_string):
-   counts = dict()
-   words = query_string.split()
-   for word in words:
-       if word in counts:
-           counts[word] += 1
-       else:
-           counts[word] = 1
-   counts = sorted(counts.items(), key=lambda item: item[1])
-   return counts
-word_count = word_count(lemmatized_text)
-print(word_count[-40:])
+def word_count(text):
+  counts = dict()
+  words = text.split()
+  for word in words:
+    if word in counts:
+      counts[word] += 1
+    else:
+      counts[word] = 1
+  counts = sorted(counts.items(), key=lambda item: item[1])
+  return counts
+print("4. sorting counted words")
+print("writing file...")
+with open("wordCount.txt", "w") as f:
+  f.write(str(word_count(lemmatized_text)))
--- greek-nlp/tokenizer/lemma_generator.py
+++ greek-nlp/tokenizer/lemma_generator.py
@@ -1,18 +1,49 @@
-
-lemmata = [("πατηρ", "πατρος"), ("ανηρ", "ανδρος"), ("βροτος", "βροτου")]
+from tokenizer import dediacritcializer as de
 
 endingsK = ["ι", "ος", "α", "ες", "ων", "σι", "σιν", "ας"]
 endingsOm = ["ου", "ω", "ον", "οι", "ων", "οις", "ους"]
 endingsOn = ["ου", "ω", "ον", "α", "ων", "οις"]
+endingsAa = ["αν", "αι", "ων", "αις", "ας"]
+endingsAe = ["ης", "ην", "αι", "ων", "αις", "ας"]
 
-
-lemmata_list = []
-
-
-def replaceLemmata(text, lemmata_list):
-  for lemmata in lemmata_list:
-    for lemma in lemmata:
-      print(lemma)
-      print(lemmata[0])
-      text = text.replace(lemma, lemmata[0])
-  return text
+def fillLemmata(lemmata): # lemmata : list
+  lemmata_list = []
+  for lemma in lemmata:
+    nom = lemma[0]
+    gen = lemma[1]
+    nom = de.dediacriticalizer(nom)
+    gen = de.dediacriticalizer(gen)
+    stem = gen[:-2]
+    if nom[-2:] == "ος":
+      tokens = [lemma[0]]
+      tokens.append(nom)
+      for ending in endingsOm:
+        tokens.append(stem + ending)
+      lemmata_list.append(tokens)
+    elif nom[-2:] == "ον":
+      tokens = [lemma[0]]
+      tokens.append(nom)
+      for ending in endingsOn:
+        tokens.append(stem + ending)
+      lemmata_list.append(tokens)
+    elif gen[-2:] == "ος":
+      tokens = [lemma[0]]
+      tokens.append(nom)
+      for ending in endingsK:
+        tokens.append(stem + ending)
+      lemmata_list.append(tokens)
+    elif nom[-1] == "α":
+      tokens = [lemma[0]]
+      tokens.append(nom)
+      for ending in endingsAa:
+        tokens.append(stem + ending)
+      lemmata_list.append(tokens)
+    elif nom[-1] == "η":
+      tokens = [lemma[0]]
+      tokens.append(nom)
+      for ending in endingsAe:
+        tokens.append(stem + ending)
+      lemmata_list.append(tokens)
+    else:
+      print("lemma not found")
+  return lemmata_list
