527b4b9a5a1027d62a1da272687ace909d7bc9fa
vincent clausing-lage <vincent.clausing.lage@gmail.com>
2021-12-27 00:26:36 +0100

    greek novel

--- greek-nlp/main_roman_tokenizer.py
+++ greek-nlp/main_roman_tokenizer.py
@@ -8,52 +8,52 @@ from tokenizer import noise_terms
 from tokenizer import lemma_generator as lem
 
 # PREPARE text (remove DIACRITICS)
-with open("tokenizer/input.json", "r") as f:
+with open("tokenizer/input-roman.json", "r") as f:
   text = f.read()
   text = json.loads(text)
 
-aeschylus = text_preparer.text_preparer(text[0]["text"])
-sophocles = text_preparer.text_preparer(text[1]["text"])
-euripides = text_preparer.text_preparer(text[2]["text"])
+longos = text_preparer.text_preparer(text["text"])
+# sophocles = text_preparer.text_preparer(text[1]["text"])
+# euripides = text_preparer.text_preparer(text[2]["text"])
 
 print("1. text loaded and prepared")
-with open("normalizedText.txt", "w") as f:
+with open("normalizedRomanText.txt", "w") as f:
   data = [
-    {"author": text[0]["author"], "normalized text": aeschylus},
-    {"author": text[1]["author"], "normalized text": sophocles},
-    {"author": text[2]["author"], "normalized text": euripides}
+    {"author": text["author"], "normalized text": longos}#,
+    # {"author": text[1]["author"], "normalized text": sophocles},
+    # {"author": text[2]["author"], "normalized text": euripides}
   ]
   data = json.dumps(data, ensure_ascii=False, indent=2)
   f.write(data)
 # make VARS
 authors = [
   {
-    "author": text[0]["author"],
-    "length": len(text[0]["text"]),
-    "normalized text": aeschylus,
+    "author": text["author"],
+    "length": len(text["text"]),
+    "normalized text": longos,
     "angry": 0,
     "nasty": 0,
     "affectionate": 0,
     "nice": 0
-  },
-  {
-    "author": text[1]["author"],
-    "length": len(text[1]["text"]),
-    "normalized text": sophocles,
-    "angry": 0,
-    "nasty": 0,
-    "affectionate": 0,
-    "nice": 0
-  },
-  {
-    "author": text[2]["author"],
-    "length": len(text[2]["text"]),
-    "normalized text": euripides,
-    "angry": 0,
-    "nasty": 0,
-    "affectionate": 0,
-    "nice": 0
-  }
+  }#,
+  # {
+  #   "author": text[1]["author"],
+  #   "length": len(text[1]["text"]),
+  #   "normalized text": sophocles,
+  #   "angry": 0,
+  #   "nasty": 0,
+  #   "affectionate": 0,
+  #   "nice": 0
+  # },
+  # {
+  #   "author": text[2]["author"],
+  #   "length": len(text[2]["text"]),
+  #   "normalized text": euripides,
+  #   "angry": 0,
+  #   "nasty": 0,
+  #   "affectionate": 0,
+  #   "nice": 0
+  # }
 ]
 # PREAPARE text (remove NOISE)
 print("removing noise from text")
@@ -102,7 +102,7 @@ def lemmatizeVerbs(text,verb_lemmata):
   print(verb_lemmata)
 
 
-text_total = authors[0]["normalized text"] + authors[1]["normalized text"] + authors[2]["normalized text"]
+text_total = authors[0]["normalized text"]
 def word_count(text):
   counts = dict()
   words = text.split()
@@ -115,7 +115,7 @@ def word_count(text):
   return counts
 print("4. sorting counted words")
 print("writing file...")
-with open("wordCount.txt", "w") as f:
+with open("wordCountRoman.txt", "w") as f:
   f.write(str(word_count(text_total)))
 
 def findToken(text, query_list):
@@ -136,6 +136,6 @@ for author in authors:
   print(author["author"] + " done")
 
 print("writing file...")
-with open("resultData.json", "w") as f:
+with open("resultRomanData.json", "w") as f:
   output = json.dumps(authors, ensure_ascii=False, indent=2)
   f.write(output)
